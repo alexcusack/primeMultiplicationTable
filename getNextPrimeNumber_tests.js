c9a38566754e195403476e17ed8ff871d6836838
Alex Cusack <cusackalex@gmail.com>
2015-10-21 13:38:34 -0700

    getNextPrimeNumber generator, move tests to own files, write test for getNextPrimeNumber

---
+++ getNextPrimeNumber_tests.js
@@ -0,0 +1,21 @@
+import { getNextPrimeNumber } from './primes'
+
+;[
+  {
+    name: 'getNextPrimeNumber test, currentPrime 2:',
+    input: [[2, 3, 4, 5, 6], [3]],
+    expected: 2,
+  },
+].forEach((td) => {
+  const primes = getNextPrimeNumber(td.input[0], td.input[1])
+  const actual = primes.next().value
+  const pass = actual === td.expected
+  if (pass) {
+    console.log('passed:', td.name)
+  } else {
+    console.log('failed:', td.name)
+    console.log('actual:', actual)
+    console.log('expected:', td.expected)
+    process.exit()
+  }
+})
